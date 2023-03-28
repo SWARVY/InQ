1663d6dcf47af6e4001d0a171d726f14265ca6b5
SWARVY <swarvy0826@naver.com>
2023-03-28 20:35:53 +0900

    add : 최초 틀 작성

---
+++ inq/src/App.js
@@ -0,0 +1,14 @@
+import { RecoilRoot } from 'recoil';
+import './App.css';
+
+function App() {
+	return (
+		<RecoilRoot>
+			<div className="App">
+				<h1>hello</h1>
+			</div>
+		</RecoilRoot>
+	);
+}
+
+export default App;
